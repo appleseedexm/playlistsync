b68cdd9e788aef09ca7d2a393df099c97af502a1
appleseedexm <appleseedexm@gmail.com>
2024-01-25 01:23:52 +0100

    setup simple get request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
-fn main() {
+use http_body_util::Empty;
+use hyper::{body::Bytes, Request};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("Hello, world!");
 
+    let url = "http://httpbin.org/ip".parse::<hyper::Uri>()?;
+    let host = url.host().expect("Cannot get host");
+    let port = url.port_u16().unwrap_or(80);
 
+    let address = format!("{}:{}", host, port);
 
+    let stream = TcpStream::connect(address).await?;
+
+    let io = TokioIo::new(stream);
+
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+
+    tokio::task::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let authority = url.authority().unwrap().clone();
+
+    let req = Request::builder()
+        .uri(url)
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+
+    let res = sender.send_request(req).await?;
+
+    println!("Response: {}", res.status());
+
+    Ok(())
 }
